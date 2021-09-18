0c0825ee582fd3f5444f353ddc7d35cba0dee796
Justin <justin.leung.ch@gmail.com>
2021-09-18 11:15:21 -0400

    feextension

--- client/src/bg/background.js
+++ client/src/bg/background.js
@@ -4,6 +4,22 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+//https://youtube.com pre much
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?youtube\.com/;
+
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
+
+// When the browser-action button is clicked...
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // ...check the URL of the active tab against our pattern and...
+    if (urlRegex.test(tab.url)) {
+        // ...if it matches, send a message specifying a callback too
+        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
+    }
+});
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
---
+++ client/src/content/content.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    // If the received message has the expected format...
+    if (msg.text === 'report_back') {
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        sendResponse(document.all[0].outerHTML);
+    }
+});
